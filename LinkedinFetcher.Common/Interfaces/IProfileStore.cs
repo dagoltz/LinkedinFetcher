23b2a8490b146a808b97775882d1f4569fd111ba
Daniel Goltz <Daniel Goltz>
2016-06-28 18:22:27 +0300

    add data provider interfaces

---
+++ LinkedinFetcher.Common/Interfaces/IProfileStore.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using LinkedinFetcher.Common.Models;
+
+namespace LinkedinFetcher.Common.Interfaces
+{
+    public interface IProfileStore
+    {
+        void Store(Profile profile);
+        IEnumerable<Profile> Search(SearchParameters parameters);
+    }
+}
---
+++ LinkedinFetcher.Common/Models/SearchParameters.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace LinkedinFetcher.Common.Models
+{
+    public class SearchParameters
+    {
+        public string Name { get; set; }
+        public string CurrentTitle { get; set; }
+        public string CurrentPosition { get; set; }
+        public string Summary { get; set; }
+        public List<string> Skills { get; set; }
+    }
+}
